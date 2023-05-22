4ddfdb702b36a194b997d0312722d622fb41c70a
yusuf <sipul.ysf@gmail.com>
2023-05-22 15:08:52 +0700

    day4

---
+++ js/blog.js
@@ -0,0 +1,28 @@
+const data =[
+    {
+        name: 'Blog 1'
+    },
+    {
+        name: 'Blog 2'
+    },
+]
+
+// get template
+const foodTemplate = document
+  .querySelector("#food-template")
+  .cloneNode(true).content;
+
+// get food list
+const foodList = document.querySelector(".foods__list");
+
+const render = () => {
+  data.forEach((food) => {
+    const foodElement = foodTemplate.cloneNode(true);
+    // fill the template with data
+    foodElement.querySelector(".foods__item-name").textContent = food.name;
+
+    foodList.appendChild(foodElement);
+  });
+};
+
+render();

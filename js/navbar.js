fc5776ee5be014ab578a66d0ce41aa03820fe733
yusuf <sipul.ysf@gmail.com>
2023-05-25 13:55:43 +0700

    My project

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+let header = document.getElementById("myDIV");
+let btns = header.getElementsByClassName("nav-menu-item a");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}

ae5271152c0ed017f779282d48d5eb82a7a9710a
yusuf <sipul.ysf@gmail.com>
2023-06-03 20:43:34 +0700

    blog

--- js/blog.js
+++ js/blog.js
@@ -26,3 +26,21 @@
 // };
 
 // render();
+
+// document.getElementsByClassName("my-nice-class").style[0] = "max-width: 40%;max-height: 40%;"
+
+function myFunction() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
